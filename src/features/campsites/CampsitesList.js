6da181519f12fabf45868d23b2302f6ae4ac88a6
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-01-11 11:19:19 +0700

    CampsiteDetailPage with useParams

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,7 +2,7 @@ import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
 
-const CampsitesList = ({ setCampsiteId }) => {
+const CampsitesList = () => {
     const campsites = selectAllCampsites();
 
     return (
@@ -11,8 +11,7 @@ const CampsitesList = ({ setCampsiteId }) => {
                 return (<Col 
                             md='5' 
                             className='m-4' 
-                            key={campsite.id}
-                            onClick={() => setCampsiteId(campsite.id)}>
+                            key={campsite.id}>
                             <CampsiteCard campsite={campsite}/>
                         </Col>);
             })}
