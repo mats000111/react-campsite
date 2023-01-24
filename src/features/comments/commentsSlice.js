3f4649ddf9f714918698aaa73bf27c5ce1a65ea4
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-01-24 20:55:23 +0700

    Campsites useSelector

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,4 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { COMMENTS } from "../../app/shared/COMMENTS";
+
+const initialState = {
+    commentsArray: COMMENTS
+}
 
 export const selectCommentsByCampsiteId = (campsiteId) => {
     return COMMENTS.filter((comment) => comment.campsiteId === parseInt(campsiteId));
