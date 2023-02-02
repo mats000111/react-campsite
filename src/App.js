a11f329ba93914c22f9a3aed34b583651aaae1ab
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-02-02 19:23:55 +0700

    Async Partners

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import AboutPage from "./pages/AboutPage";
+import { fetchPartners } from "./features/partners/partnersSlice";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCampsites());
+    dispatch(fetchPartners());
   }, [dispatch]);
 
   return (
