39e775796d90e38d74924f29fae17f72ca206f73
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-02-02 20:00:59 +0700

    Async Promotions

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from './components/Footer';
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import AboutPage from "./pages/AboutPage";
 import { fetchPartners } from "./features/partners/partnersSlice";
+import { fetchPromotions } from "./features/promotions/promotionsSlice";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,6 +19,7 @@ function App() {
   useEffect(() => {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
+    dispatch(fetchPromotions());
   }, [dispatch]);
 
   return (
